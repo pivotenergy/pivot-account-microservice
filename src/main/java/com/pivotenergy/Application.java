8c95ee996e4fe8b918cd46b8d1ec30f629e48f20
William Gayton <wgayton@me.com>
2019-05-13 19:25:57 +0200

    - code optimization

---
+++ src/main/java/com/pivotenergy/Application.java
@@ -0,0 +1,17 @@
+package com.pivotenergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
+
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(Application.class, args);
+    }
+}
